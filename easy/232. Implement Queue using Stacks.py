a3ae0d5ad32efb3aa4e3260bfe8e734bed498c58
madihanazir <madihan541@gmail.com>
2024-11-19 08:56:29 +0530

    stack

---
+++ easy/232. Implement Queue using Stacks.py
@@ -0,0 +1,40 @@
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+        
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+        
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                # removing the value at the top of the stack
+                self.s2.append(self.s1.pop())
+        # return that value
+        return self.s2.pop()
+        
+
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                # removing the value at the top of the stack
+                self.s2.append(self.s1.pop())
+        # return the value at the top of s2
+        return self.s2[-1]
+        
+
+    def empty(self) -> bool:
+         return len(self.s1) == 0 and len(self.s2) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
