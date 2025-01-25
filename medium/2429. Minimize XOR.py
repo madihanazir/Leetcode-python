7afc746fe57eca50c45bce09aedf67ce699903b3
madihanazir <madihan541@gmail.com>
2025-01-25 15:51:31 +0530

    bit manipulation

---
+++ medium/2429. Minimize XOR.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        def count_bits(n):
+            res=0
+            while n>0:
+                res += 1 & n
+                n = n>>1
+            return res
+        cnt1, cnt2= count_bits(num1), count_bits(num2)
+        x= num1
+        i=0
+        while cnt1 > cnt2:
+            if x & (1<<i):
+                cnt1 -=1
+                x= x ^ (1<<i)
+            i += 1
+        while cnt1 < cnt2:
+            if x & (1<<i)==0:
+                cnt1 += 1
+                x= x | (1<<i)
+            i += 1
+        return x
+
+      
